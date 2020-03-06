c6bfc37b2ce4c80061f63be55a23f708abf440bc
James O'Rourke <devbopo.joe@gmail.com>
2020-03-06 01:11:00 +0000

    heroku test

---
+++ configs/firebase.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+  return {
+    apiKey: "AIzaSyDnSLJ5GgoNeIeugyY7PqyMQu291lu-yi4",
+    authDomain: "iosappfyp.firebaseapp.com",
+    databaseURL: "https://iosappfyp.firebaseio.com",
+    projectId: "iosappfyp",
+    storageBucket: "iosappfyp.appspot.com",
+    messagingSenderId: "511167822434",
+    appId: "1:511167822434:web:a4dd9f70f4adeb200eaf3b",
+  }
+};
